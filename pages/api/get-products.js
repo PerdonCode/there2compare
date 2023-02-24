9d022a449ecabd0523b60b8e7a81fa045d3b7b5c
nielsperdon <niels.perdon@gmail.com>
2023-02-24 12:48:52 +0100

    fix npm run build

--- pages/api/get-products.js
+++ pages/api/get-products.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
 		const { data } = await api.get(
 			'products',
 			{
-				per_page: perPage || 50
+				per_page: perPage || 3
 			}
 		);
 		
---
+++ src/utils/constants/products.js
@@ -0,0 +1,27 @@
+const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+
+const api = new WooCommerceRestApi({
+	url: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL,
+	consumerKey: process.env.WC_CONSUMER_KEY,
+	consumerSecret: process.env.WC_CONSUMER_SECRET,
+	version: "wc/v3"
+});
+
+/**
+ * Get Products.
+ *
+ * Endpoint /api/get-products or '/api/get-products?perPage=2'
+ *
+ * @param req
+ * @param res
+ * @return {Promise<void>}
+ */
+export const getProductsData = async ( perPage) =>  {
+	
+	return await api.get(
+        'products',
+        {
+            per_page : perPage || 50,
+        },
+    );
+};
