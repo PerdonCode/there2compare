71156e27259df43cdf1b93d8f92c9006625a15f9
nielsperdon <niels.perdon@gmail.com>
2023-02-25 20:08:39 +0100

    small changes

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -8,23 +8,19 @@ const Products = ({ products }) => {
 	if ( isEmpty( products ) || !isArray( products ) ) {
 		return null;
 	}
-	
 	return (
-        <div className="flex mx-auto flex-wrap items-center max-w-6xl overflow-hidden border rounded space-between">
-            
-			
+        <div className="flex mx-auto flex-wrap i max-w-5xl overflow-hidden border rounded space-between  ">
 			{ products.length ? products.map( product => {
 				const img = product?.images?.[0] ?? {};
 				return (
-                    <div key={ product?.id } className="picture w-full overflow-hidden sm:w-1/2 md:w-1/3 xl:w-1/4 ">
+                    <div key={ product?.id } className="w-full border overflow-hidden sm:w-1/2 md:w-1/3 xl:w-1/3  ">
 						<Link href={`/product/ ${product?.slug ?? ''}`}>
-
                             <Image
                                 sourceurl={ img?.src ?? '' }
                                 alttext={ img?.alt ?? ''}
                                 title={ product?.name ?? '' }
-                                width="380"
-                                height="380"
+                                width="150"
+                                height="150"
                             />
                              <h3 className="font-bold uppercase">{ product?.name ?? '' }</h3>
                              <div dangerouslySetInnerHTML={{ __html: sanitize( product?.price_html ?? '' ) }}/>
@@ -32,7 +28,6 @@ const Products = ({ products }) => {
 					</div>
                 );
 			} ) : null }
-		
 		</div>
     );
 }
